128d2362f11ca5b616796951a77374082f294d33
Rudrank Riyam <rudrankriyam@gmail.com>
2026-05-30 11:58:42 +0000

    Update asc to 1.7.0

--- Formula/asc.rb
+++ Formula/asc.rb
@@ -12,10 +12,10 @@ class Asc < Formula
   on_macos do
     if Hardware::CPU.arm?
       url 'https://github.com/rorkai/App-Store-Connect-CLI/releases/download/1.7.0/asc_1.7.0_macOS_arm64'
-      sha256 '37302b31ef868664158948fd4caa411c8ca673bbb0edcbc510731572f2aee418'
+      sha256 '502185037bb57112ca3fdcc60c7bc1015c1ed04ea1e7b27656cd699a5f81368b'
     else
       url 'https://github.com/rorkai/App-Store-Connect-CLI/releases/download/1.7.0/asc_1.7.0_macOS_amd64'
-      sha256 '2b8a275ce0846dab470207e3c599c057d45aa861b7720dd306c26f5ecb378edf'
+      sha256 '630df77731b1b33f2c21adfe762a049b5d03fa05584350914f7d8c16005c0535'
     end
   end
 
